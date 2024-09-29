f9579ea53b02363e3e5598d280a6da17a81f2b0d
razanalsunaidi <razanalsunedi@gmail.com>
2024-09-29 16:08:26 +0300

    Initial Commit

---
+++ keepFresh/keepFreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepFreshApp.swift
+//  keepFresh
+//
+//  Created by Razan Alsunaidi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct keepFreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
